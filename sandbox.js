8e8ca5e5ca41af3c9531da427d4342edd746aa16
Sanjit-Jeevanand <75569049+Sanjit-Jeevanand@users.noreply.github.com>
2021-06-17 20:51:10 +0530

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,28 @@
+let age = 18;
+let year = 2021;
+console.log(age, year);
+age = 19;
+const points = 100;
+console.log(points);
+console.log('Hello World');
+let email = 'me@gmail';
+console.log(email);
+
+let firstName = 'Sanjit';
+let lastName = 'Jeevanand';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+let result = fullName.slice(0,6);
+console.log(result);
+result = fullName.replace('a','i');
+console.log(result);
+// ** stands for raised to 
+// u can use 'Firstname is ${firstName}' for strings
+let html = `
+ <h2>Batman</h2>
+ <p>is a Bat who is a Man</p>
+
+`
+let arr = [1,2,3,'bat'];
+arr = arr.join(',');
+console.log(arr);
